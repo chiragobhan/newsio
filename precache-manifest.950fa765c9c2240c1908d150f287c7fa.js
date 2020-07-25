fdf38dbe0a117b9d205753dbe3f70a0c2fc20a0b
Chirag Obhan <32812640+chiragobhan@users.noreply.github.com>
2020-07-26 01:25:31 +0530

    Updates

---
+++ precache-manifest.950fa765c9c2240c1908d150f287c7fa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34cc3cf58572f4b3a3bf85587da3964c",
+    "url": "/newsio/index.html"
+  },
+  {
+    "revision": "4a0ddcc3cd821dac9c3f",
+    "url": "/newsio/static/css/3.4c4477be.chunk.css"
+  },
+  {
+    "revision": "1b0801fa5c6ca52f1d37",
+    "url": "/newsio/static/css/main.d5b04bae.chunk.css"
+  },
+  {
+    "revision": "be87c22669d4a19d1203",
+    "url": "/newsio/static/js/0.a8f237ce.chunk.js"
+  },
+  {
+    "revision": "e2a608f3b392ed4f1f89ddb34b2dbcda",
+    "url": "/newsio/static/js/0.a8f237ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0ddcc3cd821dac9c3f",
+    "url": "/newsio/static/js/3.16c07953.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/newsio/static/js/3.16c07953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b45da912194993367b9",
+    "url": "/newsio/static/js/4.6bb815bb.chunk.js"
+  },
+  {
+    "revision": "9614f3804ae514bd7d0d",
+    "url": "/newsio/static/js/5.3f2591c1.chunk.js"
+  },
+  {
+    "revision": "1b0801fa5c6ca52f1d37",
+    "url": "/newsio/static/js/main.dab45940.chunk.js"
+  },
+  {
+    "revision": "b3aa3510b666c6f515fc",
+    "url": "/newsio/static/js/runtime-main.cf35b63e.js"
+  }
+]);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainLayout from './components/Layout';
-
-ReactDOM.render(<MainLayout />, document.getElementById('container'));
